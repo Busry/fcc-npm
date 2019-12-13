1a0a8d3df4c73bd321dc2864cb64014a9383d202
Glitch (locrian-woodpecker) <none>
2019-12-13 15:21:46 +0000

    🔦☕️ Updated with Glitch

--- myApp.js
+++ myApp.js
@@ -39,6 +39,12 @@ app.get("/json", function(req, res) {
 
 
 /** 8) Chaining middleware. A Time server */
+app.get('/now', function(req, res, next) {
+  req.time = new Date().toString();  // set time to now
+  next();
+  }, function(req, res) {
+  res.json({time: req.time});
+});
 
 /** 9)  Get input from client - Route parameters */
 

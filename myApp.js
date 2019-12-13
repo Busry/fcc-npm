75a0a408b2723bd7019317ce4b53d789ae586ca6
Glitch (locrian-woodpecker) <none>
2019-12-13 15:07:42 +0000

    👾💪 Updated with Glitch

--- myApp.js
+++ myApp.js
@@ -1,53 +1,54 @@
-
-var express = require('express');
+var express = require("express");
 var app = express();
+
+/** 7) Root-level Middleware - A logger */
+//  place it before all the routes !
+app.use(function(req, res, next) {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+});
 
 // --> 7)  Mount the Logger middleware here
 
-
 // --> 11)  Mount the body-parser middleware  here
 
-
 /** 1) Meet the node console. */
-console.log('Hello World')
-
+console.log("Hello World");
 
 /** 2) A first working Express Server */
-
+app.get("/", function(req, res) {
+  res.sendFile( __dirname + "/views/index.html");
+});
 
 /** 3) Serve an HTML file */
 
-
 /** 4) Serve static assets  */
-
+app.use(express.static(__dirname + "/public"));
 
 /** 5) serve JSON on a specific route */
-
+app.get("/json", function(req, res) {
+  
+  if (process.env.MESSAGE_STYLE == "uppercase"){
+    res.json({"message": "HELLO JSON"});
+  }
+});
 
 /** 6) Use the .env file to configure the app */
- 
- 
-/** 7) Root-level Middleware - A logger */
-//  place it before all the routes !
+
+
 
 
 /** 8) Chaining middleware. A Time server */
 
-
 /** 9)  Get input from client - Route parameters */
-
 
 /** 10) Get input from client - Query parameters */
 // /name?first=<firstname>&last=<lastname>
 
-  
 /** 11) Get ready for POST Requests - the `body-parser` */
 // place it before all the routes !
 
-
 /** 12) Get data form POST  */
-
-
 
 // This would be part of the basic setup of an Express app
 // but to allow FCC to run tests, the server is already active
@@ -55,4 +56,4 @@ console.log('Hello World')
 
 //---------- DO NOT EDIT BELOW THIS LINE --------------------
 
- module.exports = app;
+module.exports = app;
